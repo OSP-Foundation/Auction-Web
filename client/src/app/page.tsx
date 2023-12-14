bad148286fe4cf3ce29d5640b01ca47884e41a7b
Shebin v sajan <shebinvsajan@gmail.com>
2023-12-14 19:26:21 +0530

    footer structure

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,6 +8,8 @@ export default function Home() {
   return (
     <main>
       <Intro />
+      <Carousel />
+      <Footer />
     </main>
   )
 }
--- client/src/components/footer/index.tsx
+++ client/src/components/footer/index.tsx
@@ -1,69 +1,74 @@
+import { Tree } from '@/assets/svg'
 import React, { FunctionComponent } from 'react'
+
 
 const Footer: FunctionComponent = (): JSX.Element => {
   return (
-
-
-
-    <footer className="bg-white dark:bg-gray-900">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div className="md:flex md:justify-between">
-          <div className="mb-6 md:mb-0">
-         
-         <h1>buildin</h1>
+    <footer className="bg-white">
+       <div className='p-container md:p-container-md grid md:grid-cols-12  sm:grid-cols-none'> 
+        <div className="col-span-3 max-w-sm">
+          <div className='flex'>
+            <Tree
+              width='2rem'
+              height='2rem'
+              className='fill-primary-blue mr-4'
+            />
+            <h4 className='font-bold text-3xl'>
+              BidingX
+            </h4>
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:gap-4 sm:grid-cols-4">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
-                </li>
-                <li>
-                  <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
-                </li>
-                <li>
-                  <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <a href="#" className="hover:underline">Privacy Policy</a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline">Terms &amp; Conditions</a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <a href="#" className="hover:underline">Privacy Policy</a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline">Terms &amp; Conditions</a>
-                </li>
-              </ul>
-            </div>
+          <p className='mt-2 '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati minima tempore
+          </p>
+          <div className="grid grid-cols-[auto_auto] items-center mt-5 bg-primary-bg border border-primary-border rounded-full p-2">
+            <input
+              type="text"
+              placeholder="Enter your email"
+              className="bg-transparent w-full rounded-full p-2 outline-none focus:outline-none" />
+            <button
+              type="submit"
+              className="ml-auto bg-primary-blue text-white rounded-full px-4 py-2 hover:bg-blue-700 transition duration-300"
+            >
+              Submit
+            </button>
           </div>
         </div>
-   
+        <div className="col-span-9 grid md:grid-cols-4 gap-4  sm:grid-cols-none ">
+          <div className='text-secondary-black' >
+            <h3 className='font-bold text-primary-black   mb-3'>Enterprice</h3>
+            <ul>
+              <li >About </li>
+              <li>Explore</li>
+              <li>Our service</li>
+            </ul>
+          </div>
+          <div className='text-secondary-black' >
+            <h3 className='font-bold text-primary-black   mb-3'>Enterprice</h3>
+            <ul>
+              <li >About </li>
+              <li>Explore</li>
+              <li>Our service</li>
+            </ul>
+          </div>
+          <div className='text-secondary-black' >
+            <h3 className='font-bold text-primary-black   mb-3'>Enterprice</h3>
+            <ul>
+              <li >About </li>
+              <li>Explore</li>
+              <li>Our service</li>
+            </ul>
+          </div>
+          <div className='text-secondary-black' >
+            <h3 className='font-bold text-primary-black   mb-3'>Enterprice</h3>
+            <ul>
+              <li >About </li>
+              <li>Explore</li>
+              <li>Our service</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </footer>
-
-
   )
 }
 
